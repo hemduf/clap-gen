9e121e31b0ef8bb211010e6cad7884611f569ed8
hemduf <fduhem@gmail.com>
2026-09-03 14:21:24 +0200

    style: clang-format #59 ABI RED fixture

--- tests/codegen/issue59/abi_contract.cpp
+++ tests/codegen/issue59/abi_contract.cpp
@@ -4,10 +4,12 @@
 
 namespace detail = clapgen::generated::detail;
 
-static_assert(std::is_same_v<decltype(&detail::entry_init), decltype(clap_plugin_entry_t::init)>);
-static_assert(std::is_same_v<decltype(&detail::entry_deinit), decltype(clap_plugin_entry_t::deinit)>);
 static_assert(
-    std::is_same_v<decltype(&detail::entry_get_factory), decltype(clap_plugin_entry_t::get_factory)>);
+    std::is_same_v<decltype(&detail::entry_init), decltype(clap_plugin_entry_t::init)>);
+static_assert(
+    std::is_same_v<decltype(&detail::entry_deinit), decltype(clap_plugin_entry_t::deinit)>);
+static_assert(std::is_same_v<decltype(&detail::entry_get_factory),
+                             decltype(clap_plugin_entry_t::get_factory)>);
 static_assert(std::is_same_v<decltype(&detail::factory_get_plugin_count),
                              decltype(clap_plugin_factory_t::get_plugin_count)>);
 static_assert(std::is_same_v<decltype(&detail::factory_get_plugin_descriptor),
