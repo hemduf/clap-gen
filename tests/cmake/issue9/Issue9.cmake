e814b039b0c319adca60dc256761d970c551905d
hemduf <fduhem@gmail.com>
2026-09-05 22:50:16 +0200

    test(#9): register real incremental codegen qualification

--- tests/cmake/issue9/Issue9.cmake
+++ tests/cmake/issue9/Issue9.cmake
@@ -12,3 +12,13 @@ foreach(_clapgen_issue9_mode IN ITEMS consumer incremental cross)
       -P "${_clapgen_issue9_verify}"
   )
 endforeach()
+
+add_test(
+  NAME clapgen.cmake.issue9.real-codegen
+  COMMAND
+    "${CMAKE_COMMAND}"
+    "-DCLAPGEN_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}"
+    "-DCLAPGEN_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}"
+    "-DCLAPGEN_CONFIG=$<CONFIG>"
+    -P "${CMAKE_CURRENT_LIST_DIR}/RealCodegen.cmake"
+)
