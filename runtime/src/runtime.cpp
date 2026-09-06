45f54277c2e3378c747d24d5ee48295a297fd0b8
hemduf <fduhem@gmail.com>
2026-09-06 09:57:58 +0200

    refactor: derive runtime version from canonical version

--- runtime/src/runtime.cpp
+++ runtime/src/runtime.cpp
@@ -1,7 +1,11 @@
 #include <clapgen/runtime.hpp>
 
+#ifndef CLAPGEN_VERSION
+#error "CLAPGEN_VERSION must be provided by the build system"
+#endif
+
 namespace clapgen {
 
-std::string_view runtime_version() noexcept { return "0.1.0"; }
+std::string_view runtime_version() noexcept { return CLAPGEN_VERSION; }
 
 } // namespace clapgen
